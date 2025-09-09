060b9c4ec3a437cc336be2f8559abf9fbf63d5b6
DylanStkr <146012382+DylanStkr@users.noreply.github.com>
2025-09-09 13:01:33 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import 'react-native-gesture-handler'; 
+import React from 'react'; 
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createDrawerNavigator } from '@react-navigation/drawer'; 
+import { Provider as PaperProvider } from 'react-native-paper'; 
+import MisAhorros from './screens/MisAhorros'; 
+import TodosAhorros from './screens/TodosAhorros'; 
+const Drawer = createDrawerNavigator(); 
+      
+ 
+export default function App() { 
+  return ( 
+    <PaperProvider> 
+      <NavigationContainer> 
+        <Drawer.Navigator initialRouteName="Mis Ahorros"> 
+          <Drawer.Screen name="Mis Ahorros" component={MisAhorros} /> 
+          <Drawer.Screen name="Todos los Ahorros" component={TodosAhorros} /> 
+        </Drawer.Navigator> 
+      </NavigationContainer> 
+    </PaperProvider> 
+  ); 
+} 
